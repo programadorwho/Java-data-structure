3665f9a3efd80ed3a096e9901dc0c900fbbf7581
Thiago André Cardoso Silva <50664936+programadorwho@users.noreply.github.com>
2019-07-13 19:42:20 -0300

    Entidade para a pilha: StackPushPop

---
+++ Person.java
@@ -0,0 +1,26 @@
+package Entity;
+
+public class Person {
+    private String name;
+    public static int count;
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public Person(){
+
+    }
+    public Person(String name){
+        this.name = name;
+    }
+}
+/*
+ * Aulas particulares de programação (online e presencial)
+ * (85) 999273805
+ * Instagram profissional: https://www.instagram.com/programador_who/
+ */
