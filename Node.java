d60273d686fb5e490299867b4f65a28e8ff0522e
Thiago André Cardoso Silva <50664936+programadorwho@users.noreply.github.com>
2019-07-13 19:40:11 -0300

    No para a pilha: StackPushPop

---
+++ Node.java
@@ -0,0 +1,15 @@
+package stack;
+
+import Entity.Person;
+
+public class Node {
+    Person person;
+    Node next;
+    Node before;
+}
+
+/*
+ * Aulas particulares de programação (online e presencial)
+ * (85) 999273805
+ * Instagram profissional: https://www.instagram.com/programador_who/
+ */
